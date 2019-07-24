85477908d4b0d802a4cc7687c9f98cadf8ca5dca
katyaorl <katyaorl@gmail.com>
2019-07-24 14:53:51 +0300

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+//import {render} from "react-dom";
+
+
+interface IPersonsList {
+    count: number
+    next: string
+    previous: null
+    results: Array<IPersonData>
+}
+
+interface IPersonData {
+    name: string
+    height: number
+    mass: number
+    hair_color: string
+    skin_color: string
+    eye_color: string
+    birth_year: string
+    gender: string
+    homeworld: string
+    films: string[]
+    species: string[]
+    vehicles: string[]
+    starships: string[]
+    created: string,
+    edited: string,
+    url: string
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const personList: IPersonsList = {
+        count: 1,
+        next: 'something',
+        previous: null,
+        results: []
+    }
+
+    const [persons, setPersons] = useState<IPersonsList>(personList);
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people/`)
+            .then(res => {
+                setPersons(res.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
+    return (
+        <div>
+            {persons.results}
+        </div>
+    )
 }
+
 
 export default App;
